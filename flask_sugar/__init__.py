226e51b9aa6da7616a9cb85502d96b3d2be1c246
ShangSky <t_c_y@outlook.com>
2021-08-05 22:08:43 +0800

    0.0.2 commit

--- flask_sugar/__init__.py
+++ flask_sugar/__init__.py
@@ -4,4 +4,4 @@ from flask_sugar.exceptions import RequestValidationError
 from flask_sugar.param_functions import Path, Query, Header, Cookie, Body, Form, File
 from werkzeug.datastructures import FileStorage
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
